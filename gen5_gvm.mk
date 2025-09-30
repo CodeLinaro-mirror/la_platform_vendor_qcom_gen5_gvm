70aa1dfa7b822b830e946287acab30804ac5a886
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2025-09-30 08:23:59 +0530

    gen5_gvm: Fix AVC denial errors caused by misplaced Bluetooth properties
    Issue:
    AVC denial errors observed during Bluetooth property initialization.
    
    Root Cause:
    Properties such as bt.max.hfpclient.connections were mistakenly added
    to PRODUCT_VENDOR_PROPERTIES in gen5_gvm.mk. These properties are not
    intended for the vendor partition
    
    Solution:
    Removed the misplaced properties from gen5_gvm.mk.
    
    Change-Id: I22b372c8e04f15f3c53b80c0e71b6c977fde6b4c

--- gen5_gvm.mk
+++ gen5_gvm.mk
@@ -654,23 +654,13 @@ PRODUCT_VENDOR_PROPERTIES += persist.timed.enable=true
 # 196610 is decimal for 0x30002 to report version 3.2
 PRODUCT_VENDOR_PROPERTIES += ro.opengles.version=196610
 
-# system property for maximum number of HFP client connections
-PRODUCT_VENDOR_PROPERTIES += bt.max.hfpclient.connections=1
-
 # system prop to turn on CdmaLTEPhone always
 PRODUCT_VENDOR_PROPERTIES += telephony.lteOnCdmaDevice=1
 
 #Simulate sdcard on /data/media
 PRODUCT_VENDOR_PROPERTIES += persist.fuse_sdcard=true
 
-#system prop for wipower support
-PRODUCT_VENDOR_PROPERTIES += ro.bluetooth.emb_wp_mode=false \
-                            ro.bluetooth.wipower=false
-
-PRODUCT_VENDOR_PROPERTIES += persist.vendor.service.bt.a2dp.sink=true \
-                            persist.vendor.btstack.enable.splita2dp=false \
-                            persist.vendor.service.bdroid.sibs=false \
-                            persist.bt.clock_boottime_alarm=false
+PRODUCT_VENDOR_PROPERTIES += persist.vendor.service.bdroid.sibs=false
 
 # system prop for Hardware type Automotive
 PRODUCT_VENDOR_PROPERTIES += ro.hardware.type=automotive
@@ -790,15 +780,6 @@ PRODUCT_VENDOR_PROPERTIES += persist.vendor.car.lpm=true
 
 # default wifi country code
 PRODUCT_VENDOR_PROPERTIES += ro.boot.wificountrycode=us
-
-# The property "persist.bluetooth.enablenewavrcp" is introduced in AOSP.
-# See commit e63f6d6bda16bd94d43537fc5db754a103c6a757
-# (1) If the property is set as true, it indicates that AVRCP(TG) is enabled.
-# (2) If the property is set as false, it indicates that AVRCP(CT) is enabled.
-# In Fluoride Bluetooth stack, the default value for the property is true. This is valid with Mobile SP.
-# However in Automotive SP, AVRCP(CT) is enabled in Car UI.
-# So the property should be set as false.
-PRODUCT_VENDOR_PROPERTIES += persist.bluetooth.enablenewavrcp=false
 
 # Add gsi avb keys
 PRODUCT_PACKAGES += qcar-gsi.avbpubkey
