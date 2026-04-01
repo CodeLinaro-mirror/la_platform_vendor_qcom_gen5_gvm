da9a488d61009cf3f668fcaeedd319b19996de8f
Rohit Nehra <rnehra@qti.qualcomm.com>
2026-04-01 18:25:54 +0530

    gen5_gvm : resolving selinux issues for lmk properties
    Change-Id: I6c485f959c6122958b038ff560d2d9a2f8226f54
    Signed-off-by: Rohit Nehra <rnehra@qti.qualcomm.com>

--- gen5_gvm.mk
+++ gen5_gvm.mk
@@ -732,10 +732,7 @@ PRODUCT_VENDOR_PROPERTIES += vendor.perf.gestureflingboost.enable=true
 
 #Enable ULMK properties
 PRODUCT_VENDOR_PROPERTIES += ro.lmk.kill_heaviest_task=true \
-                            ro.lmk.kill_timeout_ms=15 \
-                            ro.lmk.enhance_batch_kill=true \
-                            ro.lmk.enable_adaptive_lmk=true \
-                            ro.lmk.vmpressure_file_min=80640 \
+                            ro.lmk.kill_timeout_ms=15
 
 #Property to enable scroll pre-obtain view
 PRODUCT_VENDOR_PROPERTIES += ro.vendor.scroll.preobtain.enable=true
