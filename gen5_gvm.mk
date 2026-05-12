756c9d875e0c8a3752f1ad9256303fbd27fde1d3
Naveen Yerramsetti <lyerrams@qti.qualcomm.com>
2026-05-12 11:16:31 +0530

    [gen5_gvm_cmu] Gaurd display_settings and Input port for CMU lunch
    Guard Display setting and Input port associations for gen5_gvm_cmu lunch
    
    Change-Id: I577345824390a807471d97ef3ad8fbc7e7a52c58
    Signed-off-by: Naveen Yerramsetti <lyerrams@qti.qualcomm.com>

--- gen5_gvm.mk
+++ gen5_gvm.mk
@@ -448,10 +448,12 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     libhealthd.msm
 
-# MTMD enablement
+# MTMD enablement, for cmu lunch separate the display and input port xml
+ifeq (,$(filter gen5_gvm_cmu, $(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)$(TARGET_BOARD_DERIVATIVE_SUFFIX)))
 PRODUCT_COPY_FILES += \
     device/qcom/gen5_gvm/input-port-associations.xml:$(TARGET_COPY_OUT_VENDOR)/etc/input-port-associations.xml \
     device/qcom/gen5_gvm/display_settings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/display_settings.xml
+endif
 
 DEVICE_MANIFEST_FILE := device/qcom/gen5_gvm/manifest.xml
 DEVICE_MATRIX_FILE   := device/qcom/common/compatibility_matrix.xml
