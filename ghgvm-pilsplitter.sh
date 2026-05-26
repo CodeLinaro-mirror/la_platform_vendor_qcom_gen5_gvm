456441ad05c1ca7e498e96646717cdb560993fc0
Vijayakumar Badiger <vbadiger@qti.qualcomm.com>
2026-05-26 12:26:59 -0700

    gen5_gvm: Copy vm_bootloader image to radio directory
    Introduced script changes to copy the vm-bootloader image
    to the radio directory, enabling support for OTA package
    generation.
    
    Change-Id: I1a9dae5710ca2c49288fa8948c70045ba05d5e10
    Signed-off-by: Vijayakumar Badiger <vbadiger@qti.qualcomm.com>

--- ghgvm-pilsplitter.sh
+++ ghgvm-pilsplitter.sh
@@ -19,6 +19,7 @@ ROOT_DIR="$PWD/../../../"
 MKDTBOIMGPY_PATH=$ROOT_DIR/system/libufdt/utils/src
 #echo "$MKDTBOIMGPY_PATH"
 IMG_PATH="$PWD/../gen5-kernel"
+RADIO_PATH="$PWD/radio"
 #echo "$IMG_PATH"
 cd $IMG_PATH
 OUTPATH="$PWD/../../../out/target/product/gen5_gvm"
@@ -93,3 +94,19 @@ echo "Creating the vm-bootloader.img"
 $ROOT_DIR/out/host/linux-x86/bin/mkuserimg_mke2fs $OUTPATH/scratch/bootloader \
 	$OUTPATH/vm-bootloader.img ext4 / 8388608 \
 	--journal_size=0
+
+# Ensure the target directory exists
+if [ ! -d "$RADIO_PATH" ]; then
+    mkdir -p "$RADIO_PATH"
+fi
+
+if [ ! -f "$OUTPATH/vm-bootloader.img" ]; then
+    echo "ERROR: vm-bootloader.img not found at $OUTPATH"
+    exit 1
+fi
+
+echo "Copying vm-bootloader.img to the radio folder"
+cp -f "$OUTPATH/vm-bootloader.img" "$RADIO_PATH/" || {
+    echo "ERROR: Failed to copy vm-bootloader.img to $RADIO_PATH"
+    exit 1
+}
